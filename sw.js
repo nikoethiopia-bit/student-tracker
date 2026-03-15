279beb47ad8e3cc5b53385dfe892d94b6cbc760e
nikoethiopia-bit <nikoethiopia@gmail.com>
2026-03-15 14:08:34 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'tracker-cache-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
